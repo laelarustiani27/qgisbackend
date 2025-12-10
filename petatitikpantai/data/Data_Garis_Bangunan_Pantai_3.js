0b1cdee87ac464aab9f9fa5e7cb352c58bd6bf7d
Laela Rustiani <lelarustiani@gmail.com>
2025-12-10 20:11:26 +0700

    Add files via upload

---
+++ petatitikpantai/data/Data_Garis_Bangunan_Pantai_3.js
@@ -0,0 +1,10 @@
+var json_Data_Garis_Bangunan_Pantai_3 = {
+"type": "FeatureCollection",
+"name": "Data_Garis_Bangunan_Pantai_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "No": 2.0, "Lokasi\/Daerah": "Garis Bangunan", "Struktur\/Posisi": "Breakwater, jetty, seadike, seawall dan groin", "Keterangan": "Batu\/beton sepanjang 2,1 km", "Persentase": "63%" }, "geometry": { "type": "LineString", "coordinates": [ [ 95.282299429660853, 5.558347221314853 ], [ 95.295563997751358, 5.569391265090232 ] ] } },
+{ "type": "Feature", "properties": { "No": 4.0, "Lokasi\/Daerah": "Garis Bangunan", "Struktur\/Posisi": "Breakwater dan seawall", "Keterangan": "Batu\/beton sepanjang 1,26 km", "Persentase": "60%" }, "geometry": { "type": "LineString", "coordinates": [ [ 95.332400992359226, 5.595746604886153 ], [ 95.326962802831233, 5.593236708108305 ] ] } },
+{ "type": "Feature", "properties": { "No": 6.0, "Lokasi\/Daerah": "Garis Bangunan", "Struktur\/Posisi": "Jetty, seadike dan groin", "Keterangan": "Batu\/beton sepanjang 2,2 km", "Persentase": "25%" }, "geometry": { "type": "LineString", "coordinates": [ [ 95.349657219480662, 5.606688217815876 ], [ 95.350259545918348, 5.602562792863147 ], [ 95.351418892730749, 5.597950667240545 ], [ 95.34843981828665, 5.597538457577807 ], [ 95.346863535728886, 5.605973859127068 ] ] } }
+]
+}
