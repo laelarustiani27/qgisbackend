e08e39d73625f653cdb053e7177878669a56e751
Laela Rustiani <lelarustiani@gmail.com>
2025-12-10 21:04:17 +0700

    Add files via upload

---
+++ petatematik/data/Data_Titik_Pantai_2.js
@@ -0,0 +1,10 @@
+var json_Data_Titik_Pantai_2 = {
+"type": "FeatureCollection",
+"name": "Data_Titik_Pantai_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "No": 1.0, "Lokasi\/Daerah": "Pantai", "Struktur\/Posisi": "Pantai Ulee Lheue", "Keterangan": "Area pelabuhan utama", "Persentase": "�" }, "geometry": { "type": "Point", "coordinates": [ 95.2889, 5.5625 ] } },
+{ "type": "Feature", "properties": { "No": 3.0, "Lokasi\/Daerah": "Pantai", "Struktur\/Posisi": "Pantai Syiah Kuala", "Keterangan": "Pesisir terlindungi struktur batu\/beton", "Persentase": "�" }, "geometry": { "type": "Point", "coordinates": [ 95.3296, 5.5939 ] } },
+{ "type": "Feature", "properties": { "No": 5.0, "Lokasi\/Daerah": "Pantai", "Struktur\/Posisi": "Pantai Alue Naga", "Keterangan": "10 km dari pusat kota", "Persentase": "�" }, "geometry": { "type": "Point", "coordinates": [ 95.345864079133079, 5.604531280995579 ] } }
+]
+}
